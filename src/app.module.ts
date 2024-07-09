63a3c53a625e3c92cfcdad711e64db52666f938b
김용민 <dydals3440@gmail.com>
2024-07-09 20:25:16 +0900

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import config from './configs/config';
 
+console.log(process.env.NODE_ENV);
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
+      load: [config],
+      cache: true,
+      expandVariables: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
